b47685e7315f07a66bf17aa5ac7450d219a9cf9a
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-05 15:35:43 +0530

    home page

---
+++ src/main/java/com/nest/employeeappbackend/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.nest.employeeappbackend.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/")
+    public String HomePage(){
+        return "Welcome to Employee Home Page";
+    }
+
+}
