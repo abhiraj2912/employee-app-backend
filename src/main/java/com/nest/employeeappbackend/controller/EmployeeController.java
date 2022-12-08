f9e34da46a17810052771ea2df5ddeece6240f01
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-08 12:17:07 +0530

    delete

--- src/main/java/com/nest/employeeappbackend/controller/EmployeeController.java
+++ src/main/java/com/nest/employeeappbackend/controller/EmployeeController.java
@@ -35,6 +35,26 @@ public class EmployeeController {
         return (List<Employee>) dao.findAll();
     }
 
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/search", consumes = "application/json", produces = "application/json")
+    public List<Employee> SearchEmployee(@RequestBody Employee e){
+        String empcode=String.valueOf(e.getEmpCode());
+        System.out.println(empcode);
+
+        return (List<Employee>) dao.SearchEmployee(e.getEmpCode());
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/delete", consumes = "application/json", produces = "application/json")
+    public HashMap<String, String> DeleteEmployee(@RequestBody Employee e){
+        String empid=String.valueOf(e.getId());
+        System.out.println(empid);
+        dao.deleteEmployee(e.getId());
+        HashMap<String,String> map = new HashMap<>();
+        map.put("status","success");
+        return map;
+    }
+
 
 
 
